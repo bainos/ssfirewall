20bf2e1c84e42006008359c5e06cde7436db6fe1
Jacopo Binosi <b4inoz@gmail.com>
2014-02-22 23:48:14 +0100

    Added some logging. Fixed ssh lock out

--- iptables-host-basic.sh
+++ iptables-host-basic.sh
@@ -13,6 +13,8 @@ IPT_RESTORE=/sbin/iptables-restore
 IPT_SAVE=/sbin/iptables-save
 EMPTY_RULES=./iptables-empty.rules
 
+jLOG='-m limit --limit 5/m --limit-burst 10 -j LOG --log-level info --log-prefix'
+
 HOST='192.168.7.4'
 IF_HOST='bond0'
 SUBNET_A='10.0.2.0/24'
@@ -33,17 +35,22 @@ $IPT_RESTORE < $EMPTY_RULES
 $IPT -N TCP
 $IPT -N UDP
 $IPT -N IN_SSH
+$IPT -N LOGDROP
 # Basic policy
 $IPT -P FORWARD DROP
 $IPT -P OUTPUT ACCEPT
 $IPT -P INPUT DROP
 # Input global rules
+$IPT -A LOGDROP $jLOG '(SSFW)[LOGDROP]: '
+$IPT -A LOGDROP -j DROP
 $IPT -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 $IPT -A INPUT -i lo -j ACCEPT
 $IPT -A INPUT -m conntrack --ctstate INVALID -j DROP
 $IPT -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+$IPT -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -j IN_SSH
 $IPT -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 $IPT -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+#$IPT -A INPUT -j LOGDROP
 $IPT -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
 $IPT -A INPUT -p tcp -j REJECT --reject-with tcp-rst
 $IPT -A INPUT -j REJECT --reject-with icmp-proto-unreachable
@@ -53,13 +60,16 @@ $IPT -A INPUT -j REJECT --reject-with icmp-proto-unreachable
 # allow for three connection packets in ten seconds. Further tries in that time
 # will blacklist the IP. The next rule adds a quirk by allowing a total of four
 # attempts in 30 minutes.
-$IPT -A INPUT -p tcp --dport ssh -m conntrack --ctstate NEW -j IN_SSH
 $IPT -A IN_SSH -m recent --name sshbf --rttl --rcheck --hitcount 3 --seconds 10 -j DROP
-$IPT -A IN_SSH -m recent --name sshbf --rttl --rcheck --hitcount 4 --seconds 1800 -j DROP 
-$IPT -A IN_SSH -m recent --name sshbf --set -j ACCEPT
+$IPT -A IN_SSH -m recent --name sshbf --rttl --rcheck --hitcount 4 --seconds 1800 -j LOGDROP 
+$IPT -A IN_SSH $jLOG '(SSFW)[IN_SSH]: '
+$IPT -A IN_SSH -m recent --name sshbf --set -j TCP
+#$IPT -A IN_SSH -m recent --name sshbf --set -j ACCEPT
 # TCP and UDP chains
 # Allow access to Proxmox web interface:
 $IPT -A TCP -d $HOST -p tcp -m multiport --dport 8006,5900 -j ACCEPT
+# Allow SSH connections
+$IPT -A TCP -d $HOST -p tcp --dport ssh -j ACCEPT
 # To accept incoming TCP connections on port 80 for a web server:
 #$IPT -A TCP -p tcp --dport 80 -j ACCEPT
 # To accept incoming TCP connections on port 443 for a web server (HTTPS):
@@ -83,11 +93,14 @@ $IPT -A FORWARD -j REJECT --reject-with icmp-host-unreach
 $IPT -P FORWARD DROP
 # Give access to the internet
 $IPT -A fw-interfaces -i $IF_A -j ACCEPT
-$IPT -A fw-interfaces -i $IF_HOST -o $IF_A -j ACCEPT
+$IPT -A fw-interfaces -p tcp -i $IF_HOST -o $IF_A -d $SUBNET_A -j ACCEPT
 $IPT -t nat -A POSTROUTING -s $SUBNET_A -o $IF_HOST -j MASQUERADE
+#$IPT -A fw-interfaces -i $IF_B -j ACCEPT
 #$IPT -A fw-interfaces -i $IF_B -j ACCEPT
 #$IPT -t nat -A POSTROUTING -s $SUBNET_B -o $IF_HOST -j MASQUERADE
 # Set up POSTROUTING chain
+#$IPT -A fw-open -d 10.0.2.101 -p tcp --dport 22 -j ACCEPT
+#$IPT -t nat -A PREROUTING -i $IF_HOST -p tcp --dport 22 -j DNAT --to 10.0.2.101
 #$IPT -A fw-open -d 192.168.0.5 -p tcp --dport 22 -j ACCEPT
 #$IPT -t nat -A PREROUTING -i ppp0 -p tcp --dport 22 -j DNAT --to 192.168.0.5
 #$IPT -A fw-open -d 192.168.0.6 -p tcp --dport 80 -j ACCEPT
